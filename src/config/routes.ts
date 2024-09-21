7daecca026abab7fcc929eff360cc4779e07341d
Felpasw <felipecavalcantelacerda@hotmail.com>
2024-09-21 15:56:29 -0300

    ajuste de erros

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -14,7 +14,7 @@ const router = express.Router()
 
 router.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', `*`)
-  r//es.header('Access-Control-Allow-Credentials', true)
+  //res.header('Access-Control-Allow-Credentials', true)
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   res.header('Access-Control-Allow-Headers', '*')
   router.use(cors({ origin: `*` }))
